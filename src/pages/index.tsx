a0f2ee4fab4b9373b8747eede0e5fe0b735dec5d
Faneva Anjaratiana <tianafaneva@gmail.com>
2023-02-10 16:59:56 +0300

    ki

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,7 @@ export  const getServerSideProps: GetServerSideProps = async (context) => {
   const apiUrl = `${origin}/api/city`;
   const {data} = await Axios.get(apiUrl);
   
+  
 
   return {
     props: {
